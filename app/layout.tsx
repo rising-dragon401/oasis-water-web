35fe0ef3997c5eb7de6dd77a7e57025c63699985
rising-dragon401 <risingdragon401@gmail.com>
2023-11-12 17:53:21 -0800

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={cn(lato.className, 'bg-background mx-auto max-w-6xl')}>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>" />
+      <link
+        rel="apple-touch-icon"
+        href="/apple-icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
+
       <body>{children}</body>
     </html>
   )
