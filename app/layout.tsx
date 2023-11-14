132901392502ca37a0a48ee18535c5af7ba78980
rising-dragon401 <risingdragon401@gmail.com>
2023-11-14 11:13:34 -0800

    opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,40 @@ import cn from 'classnames'
 
 import '@/styles/globals.css'
 
-export const metadata = {
+const meta = {
   title: 'Oaisys',
   description: 'Your source of clean water.',
+  cardImage:
+    'https://inruqrymqosbfeygykdx.supabase.co/storage/v1/object/public/website/oaisys_open_graph.jpg',
+  robots: 'follow, index',
+  favicon: '/favicon.ico',
+  url: 'https://oaisys.ai',
+  type: 'website',
+}
+
+export const metadata = {
+  openGraph: {
+    title: meta.title,
+    description: meta.description,
+    url: meta.url,
+    siteName: meta.title,
+    images: [
+      {
+        url: meta.cardImage,
+        width: 800,
+        height: 600,
+      },
+      {
+        url: meta.cardImage,
+        width: 1800,
+        height: 1600,
+        alt: 'train your ai agent',
+      },
+    ],
+
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
