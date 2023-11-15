8004a9ead2a75ed4460579ecd6fa03842eae462f
rising-dragon401 <risingdragon401@gmail.com>
2023-11-15 15:23:13 -0800

    new blogs

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -2,7 +2,9 @@
 
 import blog__bottle_water_flouride_testsMdx from './blog__bottle_water_flouride_tests.mdx.json' assert { type: 'json' }
 import blog__essentia_water_quality_reportMdx from './blog__essentia_water_quality_report.mdx.json' assert { type: 'json' }
+import blog__evian_water_quality_reportMdx from './blog__evian_water_quality_report.mdx.json' assert { type: 'json' }
 import blog__pure_life_water_quality_reportMdx from './blog__pure_life_water_quality_report.mdx.json' assert { type: 'json' }
 import blog__smartwater_quality_reportMdx from './blog__smartwater_quality_report.mdx.json' assert { type: 'json' }
+import blog__water_ingredient_listMdx from './blog__water_ingredient_list.mdx.json' assert { type: 'json' }
 
-export const allPosts = [blog__bottle_water_flouride_testsMdx, blog__essentia_water_quality_reportMdx, blog__pure_life_water_quality_reportMdx, blog__smartwater_quality_reportMdx]
+export const allPosts = [blog__bottle_water_flouride_testsMdx, blog__essentia_water_quality_reportMdx, blog__evian_water_quality_reportMdx, blog__pure_life_water_quality_reportMdx, blog__smartwater_quality_reportMdx, blog__water_ingredient_listMdx]
