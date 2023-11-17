fefcbe250b168915c84730b7cce938cb0aac5d93
rising-dragon401 <risingdragon401@gmail.com>
2023-11-17 15:35:55 -0800

    fiji

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -6,5 +6,6 @@ import blog__evian_water_quality_reportMdx from './blog__evian_water_quality_rep
 import blog__pure_life_water_quality_reportMdx from './blog__pure_life_water_quality_report.mdx.json' assert { type: 'json' }
 import blog__smartwater_quality_reportMdx from './blog__smartwater_quality_report.mdx.json' assert { type: 'json' }
 import blog__water_ingredient_listMdx from './blog__water_ingredient_list.mdx.json' assert { type: 'json' }
+import blog__fiji_water_qualityMdx from './blog__fiji_water_quality.mdx.json' assert { type: 'json' }
 
-export const allPosts = [blog__bottle_water_flouride_testsMdx, blog__essentia_water_quality_reportMdx, blog__evian_water_quality_reportMdx, blog__pure_life_water_quality_reportMdx, blog__smartwater_quality_reportMdx, blog__water_ingredient_listMdx]
+export const allPosts = [blog__bottle_water_flouride_testsMdx, blog__essentia_water_quality_reportMdx, blog__evian_water_quality_reportMdx, blog__pure_life_water_quality_reportMdx, blog__smartwater_quality_reportMdx, blog__water_ingredient_listMdx, blog__fiji_water_qualityMdx]
