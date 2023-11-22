2a1de28ab72c33761b7a4c807aca5397eda977d7
rising-dragon401 <risingdragon401@gmail.com>
2023-11-21 18:05:46 -0800

    blog

--- app/blog/[...slug]/layout.tsx
+++ app/blog/[...slug]/layout.tsx
@@ -4,7 +4,7 @@ import SubpageLayout from '@/components/home-layout'
 export default async function BlogLayout({ children }: PropsWithChildren) {
   return (
     <SubpageLayout>
-      <div className="md:px-8 px-2 py-3 md:flex justify-center flex-col items-center w-screen">
+      <div className="md:px-8 px-2 py-3 md:flex justify-center flex-col items-center w-full">
         {children}
       </div>
     </SubpageLayout>
