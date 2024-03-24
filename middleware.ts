876b1838af7488895026de7c1da25fce17f1c96b
rising-dragon401 <risingdragon401@gmail.com>
2024-03-24 16:22:04 -0700

    update matcher path

--- middleware.ts
+++ middleware.ts
@@ -25,5 +25,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/webhook).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api/webhooks/).*)'],
 }
