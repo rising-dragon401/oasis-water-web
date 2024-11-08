c16fed5e9b693c96bf7fe35feab9fcc21c407aa0
rising-dragon401 <risingdragon401@gmail.com>
2024-11-07 23:14:49 -0800

    decouple check sub

---
+++ app/api/check-subscription/route.ts
@@ -0,0 +1,113 @@
+import { getRevenueCatSubscription } from '@/utils/rev_cat'
+import { getStripeSubscription } from '@/utils/stripe'
+import {
+  getUserSubscription,
+  manageRcSubscriptionChange,
+  updateSubscriptionStatus,
+} from '@/utils/supabase-admin'
+
+export async function POST(req: Request) {
+  if (req.method === 'POST') {
+    const { uid, rcCustomerId: defaultRcCustomerId } = await req.json()
+
+    console.log('uid: ', uid)
+    console.log('defaultRcCustomerId: ', defaultRcCustomerId)
+
+    // TODO - need to check if rcCustomerId matches uid
+    // this seems like an edge case
+    // Basically only problem if a user account with a stripe id logs into device where they have an rc sub
+
+    try {
+      let provider = null
+      let subscriptionId = null
+      let dbSubData = null
+
+      if (!defaultRcCustomerId || defaultRcCustomerId === undefined) {
+        // first get user subscription data from supabase
+        // returns most recent subscription
+        dbSubData = await getUserSubscription(uid)
+
+        subscriptionId = dbSubData?.id
+
+        // Determine provider
+        if (dbSubData) {
+          // Stripe subs metadata === {}
+          // Revenue Cat subs metadata === { ..., provider: 'revenue_cat', originalAppUserId }
+          const metadata = dbSubData?.metadata as any
+
+          if (metadata?.provider === 'revenue_cat') {
+            provider = 'revenue_cat'
+          } else {
+            provider = 'stripe'
+          }
+        }
+      } else {
+        provider = 'revenue_cat'
+      }
+
+      console.log('provider: ', provider)
+
+      let subscriptionData = null
+      let status = null
+      // Now check each provider
+      if (provider === 'stripe' && subscriptionId) {
+        // get stripe subscription data
+        subscriptionData = await getStripeSubscription(subscriptionId)
+        console.log('stripe sub data: ', subscriptionData)
+
+        // Now update the subscription status in supabase
+        status = subscriptionData?.status
+        if (status) {
+          await updateSubscriptionStatus(subscriptionId, status)
+        }
+      } else if (provider === 'revenue_cat') {
+        let rcCustomerId = null
+
+        if (!defaultRcCustomerId) {
+          // First need to identitfy the rev cat id
+          const metadata = dbSubData?.metadata as any
+
+          console.log('metadata: ', JSON.stringify(metadata, null, 2))
+          rcCustomerId =
+            metadata?.originalAppUserId || metadata?.rcCustomerId || metadata?.customerId
+        } else {
+          rcCustomerId = defaultRcCustomerId
+        }
+
+        console.log('rcCustomerId: ', rcCustomerId)
+
+        subscriptionData = await getRevenueCatSubscription(rcCustomerId)
+
+        if (subscriptionData) {
+          // update or insert subscription to supabase
+          const updateRes = await manageRcSubscriptionChange(subscriptionData, rcCustomerId, uid)
+
+          if (updateRes && updateRes?.success) {
+            status = subscriptionData?.items[0]?.status
+            subscriptionId = subscriptionData?.items[0]?.id
+          }
+        }
+      }
+
+      const responseData = {
+        uid,
+        subscriptionId,
+        provider,
+        status,
+        subscriptionData,
+      }
+
+      // console.log('Subscription Data:', JSON.stringify(responseData, null, 2))
+
+      return new Response(JSON.stringify(responseData), { status: 200 })
+    } catch (err) {
+      console.log(err)
+      return new Response(JSON.stringify(err), { status: 500 })
+    }
+  } else {
+    return new Response('Method Not Allowed', {
+      headers: { Allow: 'POST' },
+      status: 405,
+    })
+  }
+}
